4eb71cd05aaed4bd2b08e1524fc2314e1116556e
JaewonIn <soou0710@snu.ac.kr>
2019-04-03 11:53:30 +0900

    2018-13743

--- practice.cpp
+++ practice.cpp
@@ -6,6 +6,6 @@ int main() {
 	/*
 	*  TODO: replace "XXXX-XXXXX" to your student number
 	*/
-	cout << "My student number is XXXX-XXXXX" << endl;
+	cout << "My student number is 2018-13743" << endl;
 	return 0
 }
